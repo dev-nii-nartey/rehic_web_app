4781b113fe96265558e55f3beacff9f28c26ba27
dev-nii-nartey <ictkemba@gmail.com>
2024-03-05 23:40:44 +0000

    add login controller function and its swagger Docs scripts

--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -10,9 +10,14 @@ import {
 import { Role } from "@prisma/client";
 import { validationResult } from "express-validator";
 import { HttpException } from "../exceptions/exception";
+import { compare, compareSync } from "bcryptjs";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../middlewares/jwt.middleware";
 
 export default class AuthController {
-  constructor() { }
+  constructor() {}
   static async register(
     request: Request,
     response: Response,
@@ -41,7 +46,33 @@ export default class AuthController {
       details: { id: newUser?.id, name: newUser?.name },
     });
   }
-  static async login(request: Request, response: Response) { 
-    
+  //Login
+  static async login(request: Request, response: Response) {
+    const result = validationResult(request);
+    if (!result.isEmpty()) {
+      throw new HttpException(
+        "Invalid Data entered",
+        unprocessableEntity,
+        result.array()
+      );
+    }
+    const { email, password } = request.body;
+    const user = await UserRepository.findByUniqueKey(email);
+    const pass = await compare(password, user!.password);
+    if (!pass) {
+      throw new HttpException("Invalid password", badRequest, [
+        {
+          msg: "The password the user entered is incorrect",
+        },
+      ]);
+    }
+    const accessToken = await generateAccessToken({ user: user?.id });
+    const refreshToken = await generateRefreshToken({ user: user?.email });
+    return response
+      .status(success_code)
+      .json({
+        message: "User credentials passed, and is able to log in successfully",
+        details: { accessToken, refreshToken },
+      });
   }
 }
