4303c899918ff6c23972d6c2a010cf0f78184912
dev-nii-nartey <ictkemba@gmail.com>
2024-03-25 00:23:03 +0000

    add events controller with initial crud operational methods

--- backend/src/app/controllers/event.controller.ts
+++ backend/src/app/controllers/event.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import EventRepository from "../models/event.model";
 import {
+  resource_code,
   success_code,
   unprocessableEntity,
 } from "../constants/status-codes-constant";
@@ -15,11 +16,7 @@ export default class EventController {
   static async create(request: Request, response: Response) {
     const result = validationResult(request);
     if (!result.isEmpty()) {
-      throw new HttpException(
-        "Invalid Data entered",
-        unprocessableEntity,
-        result.array()
-      );
+      throw new HttpException(unprocessableEntity, result.array());
     }
     const data = matchedData(request);
     const event = new EventRepository(
@@ -29,18 +26,14 @@ export default class EventController {
       data.time
     );
     const newEvent = await event.add();
-    return response.status(success_code).json(new JsonOutput(newEvent));
+    return response.status(resource_code).json(new JsonOutput(newEvent));
   }
 
   //update an event
   static async update(request: Request, response: Response) {
     const result = validationResult(request);
     if (!result.isEmpty()) {
-      throw new HttpException(
-        "Invalid Data entered",
-        unprocessableEntity,
-        result.array()
-      );
+      throw new HttpException(unprocessableEntity, result.array());
     }
     const data = matchedData(request);
     const event = new EventRepository(
@@ -50,12 +43,45 @@ export default class EventController {
       data.time
     );
     const updatedEvent = await event.update(data.id);
-    return response.status(success_code).json(new JsonOutput(updatedEvent));
+    return response.status(resource_code).json(new JsonOutput(updatedEvent));
+  }
+
+  static async searchByName(request: Request, response: Response) {
+    const result = validationResult(request);
+    if (!result.isEmpty()) {
+      throw new HttpException(unprocessableEntity, result.array());
+    }
+    const data = <{ name: "string" }>matchedData(request);
+    const event = await EventRepository.findEvent(data.name);
+    return response.status(success_code).json(new JsonOutput(event));
   }
 
   static async find(request: Request, response: Response) {
-    const data = <{ params: string }>request.params;
-    const event = await EventRepository.findEvent(data.params);
+    const result = validationResult(request);
+    if (!result.isEmpty()) {
+      throw new HttpException(unprocessableEntity, result.array());
+    }
+    const data = matchedData(request);
+    const event = await EventRepository.findEventById(+data.params);
     return response.status(success_code).json(new JsonOutput(event));
+  }
+
+  static async fetch(request: Request, response: Response) {
+    const allEvents = await EventRepository.fetchAll();
+    const count = await EventRepository.count();
+    const result = { allEvents, count };
+
+    return response.status(success_code).json(new JsonOutput(result));
+  }
+
+  static async delete(request: Request, response: Response) {
+    const result = validationResult(request);
+    if (!result.isEmpty()) {
+      throw new HttpException(unprocessableEntity, result.array());
+    }
+    const data = matchedData(request);
+    const id = parseInt(data.id);
+    const deletedEvent = await EventRepository.destroy(id);
+    return response.status(resource_code).json(new JsonOutput(deletedEvent));
   }
 }
