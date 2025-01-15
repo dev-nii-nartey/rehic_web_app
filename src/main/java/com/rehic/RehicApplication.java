0551400f72d85566531d36475c212b90e549d775
Nii Nartey <ictkemba@gmail.com>
2025-01-15 07:43:27 +0000

    initial project files

---
+++ src/main/java/com/rehic/RehicApplication.java
@@ -0,0 +1,13 @@
+package com.rehic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RehicApplication.class, args);
+    }
+
+}
